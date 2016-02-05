6f00104b729ffec47cda083cde9ffbaa21fd254d
Jaydev Mahadevan <jaydevm@gmail.com>
2016-02-05 17:12:47 -0600

    Set SCSS indent level to four spaces

--- personal/css.el
+++ personal/css.el
@@ -4,3 +4,6 @@
 (add-hook 'less-css-mode-hook
           (lambda ()
             (setq css-indent-offset 4)))
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (setq css-indent-offset 4)))
